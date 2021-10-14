f93a529d32fe73651f2f1b1467c6108365402a29
yyzbf <1251234619@qq.com>
2021-10-14 09:11:07 +0800

    完成了登陆注册

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-10 11:24:08
+ * @LastEditTime: 2021-10-14 09:07:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \第四阶段：前后端交互阶段资料新\大事件项目课程资料\day1（1-3小节）\code\assets\js\baseAPI.js
+ */
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    console.log(options.url);
 })
